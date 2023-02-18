2628ccc78630211223cacf46ed96effc9d37333f
mhela <mhela@DESKTOP-S5TRKML>
2023-02-18 02:09:41 +0200

    Add project files.

---
+++ Controllers/InventoryController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityIndian.Controllers
+{
+    [Authorize(Roles ="Admin")]
+    public class InventoryController : Controller
+    {
+        public IActionResult GetAll()
+        {
+            return View();
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using IdentityIndian.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityIndian.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
